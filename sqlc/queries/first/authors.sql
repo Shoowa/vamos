f9ce2d17dd16245fb87359e572f5149172f0b35e
Joshua Evans <x.evans@gmail.com>
2025-07-25 14:12:34 -0400

    build(database): configure & generate code

---
+++ sqlc/queries/first/authors.sql
@@ -0,0 +1,11 @@
+-- name: GetAuthor :one
+SELECT * FROM authors WHERE name = $1 LIMIT 1;
+
+-- name: ListAuthors :many
+SELECT * FROM authors ORDER BY name;
+
+-- name: CreateAuthor :execresult
+INSERT INTO authors (name, bio) VALUES ($1, $2);
+
+-- name: DeleteAuthor :exec
+DELETE FROM authors WHERE id = $1;
