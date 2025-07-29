fbc0a99fd9409ea6e3f752a75a412f2bdb4d3a49
Joshua Evans <x.evans@gmail.com>
2025-07-29 13:26:08 -0400

    test(database): read data concurrently

--- internal/data/rdbms/rdbms_int_test.go
+++ internal/data/rdbms/rdbms_int_test.go
@@ -13,15 +13,18 @@ import (
 	"vamos/internal/config"
 	. "vamos/internal/data/rdbms"
 	. "vamos/internal/testhelper"
+	"vamos/sqlc/data/first"
 
 	"github.com/jackc/pgx/v5"
 )
 
 const (
 	TIMEOUT_TEST = time.Second * 1
+	TIMEOUT_READ = time.Second * 3
 	PROJECT      = "vamos"
 	TEST_DB_POS  = 0
 	FAKE_DATA    = "_testdata/fake_data_db1.sql"
+	AUTHOR       = "Chaucer"
 )
 
 func change_to_project_root() {
@@ -86,10 +89,52 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
+// First, test opening a connection to a database.
 func Test_ConnectDB(t *testing.T) {
 	t.Setenv("APP_ENV", "DEV")
 	t.Setenv("OPENBAO_TOKEN", "token")
 	db, dbErr := ConnectDB(config.Read(), TEST_DB_POS)
 	defer db.Close()
 	Ok(t, dbErr)
+}
+
+// Second, test reading data concurrently.
+func Test_ReadingData(t *testing.T) {
+	t.Setenv("APP_ENV", "DEV")
+	t.Setenv("OPENBAO_TOKEN", "token")
+
+	db, _ := ConnectDB(config.Read(), TEST_DB_POS)
+	q := FirstDB_AdoptQueries(db)
+
+	timer, _ := context.WithTimeout(context.Background(), TIMEOUT_READ)
+
+	t.Run("Read one author", func(t *testing.T) {
+		readOneAuthor(t, q, timer)
+	})
+
+	t.Run("Read many authors", func(t *testing.T) {
+		readManyAuthors(t, q, timer)
+	})
+
+	t.Cleanup(func() { db.Close() })
+}
+
+func readOneAuthor(t *testing.T, q *first.Queries, ctx context.Context) {
+	t.Parallel()
+	timer, cancel := context.WithTimeout(ctx, TIMEOUT_TEST)
+	defer cancel()
+	result, err := q.GetAuthor(timer, AUTHOR)
+
+	Ok(t, err)
+	Equals(t, result.Name, AUTHOR)
+}
+
+func readManyAuthors(t *testing.T, q *first.Queries, ctx context.Context) {
+	t.Parallel()
+	timer, cancel := context.WithTimeout(ctx, TIMEOUT_TEST)
+	defer cancel()
+	result, err := q.ListAuthors(timer)
+
+	Ok(t, err)
+	Equals(t, 5, len(result))
 }
