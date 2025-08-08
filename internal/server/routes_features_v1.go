a64bede58062dd43239e15c7ce37d275c3eba477
Joshua Evans <x.evans@gmail.com>
2025-08-08 12:26:33 -0400

    refactor: move err handling to dedicated Handler

--- internal/server/routes_features_v1.go
+++ internal/server/routes_features_v1.go
@@ -15,12 +15,33 @@ const (
 	TIMEOUT_REQUEST    = time.Second * 1
 )
 
-func addFeaturesV1(router *http.ServeMux, b *Backbone) {
-	readAuthor := http.HandlerFunc(b.readAuthor)
-	router.HandleFunc("GET /author/{surname}", readAuthor)
+type errHandler func(http.ResponseWriter, *http.Request) error
+
+func (b *Backbone) eHand(f errHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		err := f(w, req)
+		if err != nil {
+			switch {
+			case errors.Is(err, context.Canceled):
+				b.Logger.Error("HTTP", "status", StatusClientClosed)
+			case errors.Is(err, context.DeadlineExceeded):
+				b.Logger.Error("HTTP", "status", http.StatusRequestTimeout)
+				http.Error(w, "timeout", http.StatusRequestTimeout)
+			case errors.Is(err, sql.ErrNoRows):
+				w.WriteHeader(http.StatusNoContent)
+			default:
+				b.Logger.Error("HTTP", "err", err.Error())
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		}
+	}
 }
 
-func (b *Backbone) readAuthor(w http.ResponseWriter, req *http.Request) {
+func addFeaturesV1(router *http.ServeMux, b *Backbone) {
+	router.HandleFunc("GET /author/{surname}", b.eHand(b.readAuthor))
+}
+
+func (b *Backbone) readAuthor(w http.ResponseWriter, req *http.Request) error {
 	metrics.ReadAuthorCounter.Inc()
 	surname := req.PathValue("surname")
 	timer, cancel := context.WithTimeout(req.Context(), TIMEOUT_REQUEST)
@@ -28,19 +49,9 @@ func (b *Backbone) readAuthor(w http.ResponseWriter, req *http.Request) {
 	result, err := b.FirstDB.GetAuthor(timer, surname)
 
 	if err != nil {
-		switch {
-		case errors.Is(err, context.Canceled):
-			b.Logger.Error("readAuthor", "status", StatusClientClosed)
-		case errors.Is(err, context.DeadlineExceeded):
-			b.Logger.Error("readAuthor", "status", http.StatusRequestTimeout)
-			http.Error(w, "timeout", http.StatusRequestTimeout)
-		case errors.Is(err, sql.ErrNoRows):
-			w.WriteHeader(http.StatusNoContent)
-		default:
-			b.Logger.Error("readAuthor", "err", err.Error())
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
+		return err
 	}
 
 	w.Write([]byte(result.Name))
+	return nil
 }
