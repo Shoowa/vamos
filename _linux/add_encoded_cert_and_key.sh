6046f8636f1e52be81225249218a81fc53fa3437
Joshua Evans <x.evans@gmail.com>
2025-12-17 12:40:19 -0500

    ci(secrets): reformat bash script

--- _linux/add_encoded_cert_and_key.sh
+++ _linux/add_encoded_cert_and_key.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
 curl -H "X-Vault-Token: token" \
+  http://localhost:8200/v1/secret/data/dev-app-cert \
   -H "Content-Type: application/json" \
-  --request POST --data '{"data" : {"cert": "'"$(base64 --wrap=0 /data/ca/public/app.pem)"'", "client_cert": "'"$(base64 --wrap=0 /data/ca/public/app_client.pem)"'"}}' \
-  http://localhost:8200/v1/secret/data/dev-app-cert
+  --request POST \
+  --data @- <<EOF
+{
+  "data" :
+  {
+    "cert": "$(base64 --wrap=0 /data/ca/public/app.pem)",
+    "client_cert": "$(base64 --wrap=0 /data/ca/public/app_client.pem)"
+  }
+}
+EOF
 
 curl -H "X-Vault-Token: token" \
+  http://localhost:8200/v1/secret/data/dev-app-key \
   -H "Content-Type: application/json" \
-  --request POST --data '{"data" : {"private_key": "'"$(base64 --wrap=0 /data/ca/private/app-key.pem)"'", "client_private_key": "'"$(base64 --wrap=0 /data/ca/private/app_client-key.pem)"'"}}' \
-  http://localhost:8200/v1/secret/data/dev-app-key
+  --request POST \
+  --data @- <<EOF
+{
+  "data" :
+  {
+    "private_key": "$(base64 --wrap=0 /data/ca/private/app-key.pem)",
+    "client_private_key": "$(base64 --wrap=0 /data/ca/private/app_client-key.pem)"
+  }
+}
+EOF
