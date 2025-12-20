40fd4a843a190313a8e44050a18352779a4dfb02
Joshua Evans <x.evans@gmail.com>
2025-12-20 11:42:46 -0500

    test(secrets): test method to read secrets

--- secrets/secrets_int_test.go
+++ secrets/secrets_int_test.go
@@ -13,6 +13,9 @@ import (
 
 const (
 	PASSWORD = "OpenBao123"
+	REDIS_SECRET = "dev-redis-test"
+	REDIS_PW     = "ReDiS4LiFe"
+
 )
 
 func Test_Connection(t *testing.T) {
@@ -42,5 +45,20 @@ func Test_SkeletonKeyOpenbao(t *testing.T) {
 	tok := sk.Openbao.Token()
 	Assert(t, strings.Contains(addr, "localhost"), "Lacks localhost in host address.")
 	Assert(t, strings.Contains(tok, "token"), "Lacks token from environment.")
+}
 
+func Test_ReadValueFromOpenbao(t *testing.T) {
+	t.Setenv("APP_ENV", "DEV")
+	t.Setenv("OPENBAO_TOKEN", "token")
+
+	cfg := config.Read()
+	sk := new(SkeletonKey)
+	sk.Create(cfg)
+
+	val, err := sk.ReadPathAndKey(REDIS_SECRET, "password")
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	Equals(t, REDIS_PW, val)
 }
