9971f8acb22904ed6b1576811ebb29a075bb1434
Joshua Evans <x.evans@gmail.com>
2025-12-20 10:12:54 -0500

    test(secrets): Wrap Openbao client in a new struct

--- secrets/secrets_int_test.go
+++ secrets/secrets_int_test.go
@@ -3,6 +3,7 @@
 package secrets_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/Shoowa/vamos/config"
@@ -27,4 +28,19 @@ func Test_Connection(t *testing.T) {
 	}
 
 	Equals(t, PASSWORD, pw)
+}
+
+func Test_SkeletonKeyOpenbao(t *testing.T) {
+	t.Setenv("APP_ENV", "DEV")
+	t.Setenv("OPENBAO_TOKEN", "token")
+
+	cfg := config.Read()
+	sk := new(SkeletonKey)
+	sk.Create(cfg)
+
+	addr := sk.Openbao.Address()
+	tok := sk.Openbao.Token()
+	Assert(t, strings.Contains(addr, "localhost"), "Lacks localhost in host address.")
+	Assert(t, strings.Contains(tok, "token"), "Lacks token from environment.")
+
 }
